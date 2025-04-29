fa59da4b7c66ddc37b5e14be2684b9afb579e039
SungJoon Kwon <43159901+Kwon-Sung-joon@users.noreply.github.com>
2025-04-29 13:24:55 +0900

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -87,6 +87,7 @@ class CloudWatchAlarmParser:
         return _message
 
 def get_ssm_parameters_svc(accountId):
+    #알람 발생 계정명 확인
     ssm_client = boto3.client('ssm');
     svc_name=ssm_client.get_parameters(Names=['SERVICE_NAME'])['Parameters'];
     value=svc_name[0]['Value']
@@ -98,6 +99,7 @@ def get_ssm_parameters_svc(accountId):
 
     return res[accountId]
 def get_ssm_parameters_url(channel):
+    #알람을 보낼 슬랙 채널 명 확인
     ssm_client = boto3.client('ssm');
     chnl_name=ssm_client.get_parameters(Names=['CHANNEL_NAME'])['Parameters'];
     value=chnl_name[0]['Value']
@@ -110,6 +112,7 @@ def get_ssm_parameters_url(channel):
     print("{0} WEBHOOK_URL : {1}".format(channel,res[channel]))
     return res[channel]
 def get_ssm_parameters_role(accountId):
+    #계정에 기반하여 해당 계정의 iam role로 assume role
     ssm_client = boto3.client('ssm');
     chnl_name=ssm_client.get_parameters(Names=['CW_IAM_ROLE_ARN'])['Parameters'];
     value=chnl_name[0]['Value']
@@ -122,6 +125,7 @@ def get_ssm_parameters_role(accountId):
 
 class GetResourceHookURL:
     def __init__(self,accountId):
+        #알람 발생 계정으로 assume role
         self.sts_client=boto3.client('sts');
 
         #get session to target aws account.
@@ -142,7 +146,6 @@ class GetResourceHookURL:
         aws_session_token=SESSION_KEY["aws_session_token"]
         )
 
-        #ec2_client=boto3.client('ec2');
         ec2_info=ec2_client.describe_instances(InstanceIds=[instanceId])
         for tags in ec2_info['Reservations'][0]['Instances'][0]['Tags']:
             if tags['Key'] == 'ALARM':
@@ -158,25 +161,60 @@ class GetResourceHookURL:
         aws_session_token=SESSION_KEY["aws_session_token"]
         )
 
-
-        #ec2_client=boto3.client('ec2');
         rds_info=rds_client.describe_db_instances(DBInstanceIdentifier=rdsIdentifier)
         for tags in rds_info['DBInstances'][0]['TagList']:
             if tags['Key'] == 'ALARM':
                 hook_url=get_ssm_parameters_url(tags['Value'])
                 return hook_url
-    def get_hook_url_by_elb_tags(self,targetGroup):
+    def get_hook_url_by_elb_tags(self,arns):
         #get target instance tags (Alarm tags)
         elb_client=boto3.client('elbv2',  aws_access_key_id=SESSION_KEY["aws_access_key_id"],
         aws_secret_access_key=SESSION_KEY["aws_secret_access_key"],
         aws_session_token=SESSION_KEY["aws_session_token"]
         )
-
-        tg_info=elb_client.describe_tags(ResourceArns=[targetGroup])
+        
+        tg_info=elb_client.describe_tags(ResourceArns=[arns])
         for tags in tg_info['TagDescriptions'][0]['Tags']:
+            print(tags['Key'])
             if tags['Key'] == 'ALARM':
-                hook_url=get_ssm_parameters_url("#"+tags['Value'])
+                hook_url=get_ssm_parameters_url(tags['Value'])
                 return hook_url
+
+    def get_lb_acm(self,elb):
+        #get target instance tags (Alarm tags)
+        elb_client=boto3.client('elbv2',  aws_access_key_id=SESSION_KEY["aws_access_key_id"],
+        aws_secret_access_key=SESSION_KEY["aws_secret_access_key"],
+        aws_session_token=SESSION_KEY["aws_session_token"]
+        )
+        
+        lb_info=elb_client.describe_listeners(LoadBalancerArn=elb)
+        domains=[]
+        #get https 
+        for i in lb_info['Listeners']:
+            if i['Protocol'] == "HTTPS":
+                print(i['ListenerArn'])
+                #리스너 acm 추출
+                certs=elb_client.describe_listener_certificates(ListenerArn=i['ListenerArn'])
+                #acm arn 중복 없이 추출
+                unique_arns = list({item['CertificateArn'] for item in certs['Certificates']})
+                #acm 별로 도메인 가져오기
+                for cert in unique_arns:
+                    domain=self.get_acm_domain(cert)
+                    domain_names = [item['DomainName'] for item in domain]
+                    domains.append(domain_names)
+
+        
+        return domains
+
+    def get_acm_domain(self,acm):
+        elb_client=boto3.client('acm',  aws_access_key_id=SESSION_KEY["aws_access_key_id"],
+        aws_secret_access_key=SESSION_KEY["aws_secret_access_key"],
+        aws_session_token=SESSION_KEY["aws_session_token"]
+        )
+        acm_info=elb_client.describe_certificate(CertificateArn=acm)
+        
+        return acm_info['Certificate']['DomainValidationOptions']
+        
 
 
 
@@ -185,7 +223,7 @@ def lambda_handler(event, context):
   print(json.dumps(event));
   
   ### Check Service Account and Service Channel
-  
+
   hook_url=""
   msg=json.loads(event['Records'][0]['Sns']['Message'])
   slack_data = CloudWatchAlarmParser(msg).slack_data()
@@ -200,7 +238,7 @@ def lambda_handler(event, context):
   
   elif (msg['Trigger']['Namespace'] == "AWS/VPN"):
       #VPN
-      hook_url=get_ssm_parameters_url("#aws-vpn-alert")
+      hook_url=get_ssm_parameters_url("aws-vpn-alert")
   elif msg['Trigger']['Namespace'] == "AWS/RDS":
       #RDS
       for i in msg['Trigger']['Dimensions']:
@@ -208,16 +246,37 @@ def lambda_handler(event, context):
               rds_client=GetResourceHookURL(msg['AWSAccountId']);
               hook_url=rds_client.get_hook_url_by_rds_tags(i['value'])
   elif msg['Trigger']['Namespace'] == "AWS/ApplicationELB" or msg['Trigger']['Namespace'] == "AWS/NetworkELB":
-      #ELBv2
-      for i in msg['Trigger']['Dimensions']:
-          if i['name'] == "TargetGroup":
-              elb_client=GetResourceHookURL(msg['AWSAccountId']);
-              tg_arn = "arn:aws:elasticloadbalancing:ap-northeast-2:"+msg["AWSAccountId"]+":"+i['value']
-              hook_url=elb_client.get_hook_url_by_elb_tags(tg_arn)
-  elif msg['Trigger']['Namespace'] == "AWS/DX":
-      #DX
-      hook_url=get_ssm_parameters_url("#dx_alert")
 
+      if msg['Trigger']['MetricName'] == "ClientTLSNegotiationErrorCount":
+        elb_client=GetResourceHookURL(msg['AWSAccountId']);
+        lb_arn="arn:aws:elasticloadbalancing:ap-northeast-2:"+msg["AWSAccountId"]+":loadbalancer/"+msg['Trigger']['Dimensions'][0]['value']
+        get_lb_acm_domains=elb_client.get_lb_acm(lb_arn)      
+        hook_url=elb_client.get_hook_url_by_elb_tags(lb_arn)
+        #hook_url="https://hooks.slack.com/services/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"
+        slack_data['attachments'][0]['fields'].append({
+            'title': 'LB',
+            'value': msg['Trigger']['Dimensions'][0]['value'],
+            'short': False
+        })
+        
+        result = ' '.join(item for sublist in get_lb_acm_domains for item in sublist)
+        slack_data['attachments'][0]['fields'].append({
+            'title': 'Domains',
+            'value': result,
+            'short': False
+        })
+        print(slack_data)
+        
+      else:
+        #ELBv2
+        for i in msg['Trigger']['Dimensions']:
+            if i['name'] == "TargetGroup":
+                elb_client=GetResourceHookURL(msg['AWSAccountId']);
+                tg_arn = "arn:aws:elasticloadbalancing:ap-northeast-2:"+msg["AWSAccountId"]+":"+i['value']
+                hook_url=elb_client.get_hook_url_by_elb_tags(tg_arn)
+  elif   msg['Trigger']['Namespace'] == "AWS/DX":
+      #DX
+      hook_url=get_ssm_parameters_url("aws-dx-alert")
 
   request = Request(
         hook_url, 
